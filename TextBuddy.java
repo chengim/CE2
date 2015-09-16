328e0833dab8e9320b56f222c9150684f6659d67
chengim <chengim@users.noreply.github.com>
2015-09-16 22:50:41 +0800

    Intro of TextBuddy

---
+++ TextBuddy.java
@@ -0,0 +1,15 @@
+/**
+  *Lim Chen Gim A0113645L
+  *
+  * This program TextBuddy manipulates text in a file.
+  * Users can input commands such as 'ADD', 'DELETE', 'DISPLAY', 'CLEAR',
+  * 'SORT', 'SEARCH', 'EXIT' to edit contents in a text file. This program
+  * will auto-save after every change user makes.
+  *
+  * Assumptions:
+  * 1. Inputs entered are always valid. (Otherwise program will terminate due
+  * to error as I did not implement error catching.)
+  *
+  */
+  
+  
